60447c1f6f261ef1016ae2402074403ab5785785
David Akin <akindav16@gmail.com>
2025-11-24 18:33:52 +0100

    created functions that return functions

--- main.go
+++ main.go
@@ -111,5 +111,17 @@ func bubbleSort(l []int) {
 	}
 }
 
+func plusTwo() func(int) int {
+	return func (x int) int{
+		return  x + 2
+	}
+}
+
+func plusX(x int) func(int) int {
+	return func (y int) int{
+		return  x + y
+	}
+}
+
 func main() {
 }
